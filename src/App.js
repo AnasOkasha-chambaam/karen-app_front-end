3216c150c6722299a434108a0b06223dbc02c252
AnasOkasha-chambaam <anasokashachama@gmail.com>
2023-02-09 04:51:10 +0200

    Separating the subdomain functionality

--- src/App.js
+++ src/App.js
@@ -5,32 +5,8 @@ import Showcase from "./components/Showcase";
 import background from "./imgs/background.png";
 import { Routes, Route } from "react-router-dom";
 import ZipCode from "./components/ZipCode";
-import SingleRestaurant from "./components/SingleRestaurant";
 
 function App() {
-  let subdomain;
-  const host = window.location.host,
-    hostArr = host.split(".").slice(0, host.includes("restobau") ? -1 : -2);
-  if (hostArr.length > 0 && hostArr[0] !== "www" && hostArr[0] !== "restobau") {
-    subdomain = hostArr[0];
-    console.log(subdomain);
-    return (
-      <div
-        style={{
-          background: `url(${background}) no-repeat center center/cover`,
-        }}
-      >
-        <header className="header">
-          <a href="http://restobau.at">
-            <Logo />
-          </a>
-        </header>
-        <Routes>
-          <Route path="/" element={<SingleRestaurant />} />
-        </Routes>
-      </div>
-    );
-  }
   return (
     <div
       style={{ background: `url(${background}) no-repeat center center/cover` }}
@@ -41,10 +17,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Showcase />} />
         <Route path="/restaurants/:zipcode" element={<ZipCode />} />
-        <Route
-          path="/restaurant/:zipcode/:i/:retaurantname"
-          element={<SingleRestaurant />}
-        />
       </Routes>
     </div>
   );
